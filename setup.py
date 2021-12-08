703fc1e32a7e3d49ab9c20d9da8009fdfd282ecc
tony92151 <tony92151@gmail.com>
2021-12-08 14:21:20 +0800

    fix easy install

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
 from setuptools import setup, find_packages
 import os
+
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+fedml_api_files = package_files('fedml_api')
+fedml_core_files = package_files('fedml_core')
+fedml_experiments_files = package_files('fedml_experiments')
+
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
@@ -7,9 +21,9 @@ setup(
     name="FedML",
     url="https://github.com/FedML-AI/FedML",
     packages=['fedml_api', 'fedml_core', 'fedml_experiments'],
-    package_data={'fedml_api': ['fedml_api/**/*'],
-                  'fedml_core': ['fedml_core/**/*'],
-                  'fedml_experiments': ['fedml_experiments/**/*']},
+    package_data={'fedml_api': fedml_api_files,
+                  'fedml_core': fedml_core_files,
+                  'fedml_experiments': fedml_experiments_files},
     include_package_data=True,
     python_requires=">=3.6",
 
